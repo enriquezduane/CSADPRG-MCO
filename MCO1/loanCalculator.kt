195ddf2e92d610af7dc43aa7ae761d64e3a65f4d
Shanley Valenzuela <146340503+shanleyvalenzuela@users.noreply.github.com>
2024-11-11 16:13:52 +0800

    Update loanCalculator.kt

--- MCO1/loanCalculator.kt
+++ MCO1/loanCalculator.kt
@@ -1,3 +1,9 @@
+
+/* ********************
+* Last names: Enriquez, Gideon, Valenzuela
+* Language: Kotlin
+* Paradigm(s): Object-Oriented
+ ******************** */
 class LoanCalculator(
     private var loanAmount: Double,   // Loan amount in PHP
     private var annualIR: Double,     // Annual interest rate in percentage
@@ -38,22 +44,19 @@ class LoanCalculator(
  
     fun main() {
         print("Enter loan amount (PHP): ")
-        val loanAmount = readLine()?.toDoubleOrNull() ?: 0.0
-        //val loanAmount = readln().toDouble
+        val loanAmount = readln().toDouble()
     
         print("Enter annual interest rate (%): ")
-        val annualIR = readLine()?.toDoubleOrNull() ?: 0.0
-        //val annualIR = readln().toDouble
+        val annualIR = readln().toDouble()
     
         print("Enter loan term (years): ")
-        val loanTerm = readLine()?.toIntOrNull() ?: 1
-        //val loanTerm = readln().toInt()
+        val loanTerm = readln().toInt()
     
         val calculator = LoanCalculator(loanAmount, annualIR, loanTerm)
    
         println("Loan Amount: PHP ${calculator.getLoanAmount()}")
         println("Annual Interest Rate: ${calculator.getAnnualInterestRate()}%")
-        println("Loan Term: $loanTerm years")
+        println("Loan Term: ${loanTerm*12} months")
         println("Monthly Repayment: PHP ${"%.2f".format(calculator.calculateMonthlyRepayment())}")
         println("Total Interest: PHP ${"%.2f".format(calculator.calculateTotalInterest())}")
     }
